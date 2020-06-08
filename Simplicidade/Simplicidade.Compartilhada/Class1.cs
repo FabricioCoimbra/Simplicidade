f79b693a8efe32658473251fce6c25c7af0b2aba
fabricio.coimbra <fabriciob.coimbra@gmail.com>
2020-06-08 20:55:59 -0300

    Adicionados todos os projetos vazios com template padrão

---
+++ Simplicidade/Simplicidade.Compartilhada/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Simplicidade.Compartilhada
+{
+    public class Class1
+    {
+        public Class1()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
